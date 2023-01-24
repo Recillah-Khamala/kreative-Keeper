4e529e6c3986391bab55c5282daa7db3263a8255
recillah khamala <recillahk@gmail.com>
2023-01-24 22:59:46 +0300

    Add and run Comment migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_190228) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_195847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
