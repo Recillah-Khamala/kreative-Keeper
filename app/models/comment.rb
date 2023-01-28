f2aff5686cb29627ec4510ee4086cbe0a6c40093
recillah khamala <recillahk@gmail.com>
2023-01-28 04:25:45 +0300

    Add methods to user, post, and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
+  after_save :update_commentscounter
+
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_commentscounter
+    post.update(commentscounter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
+  after_save :update_postscounter
+
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :posts
+
+  def update_posts_count
+    author.update(postscounter: author.posts.count)
+  end
+
+  def recent_comments
+    self.comments.order(created_at: :desc).limit(5)
+  end
 end
