bfcc96105f98d294939f3455cfbf8307b8a1cc04
recillah khamala <recillahk@gmail.com>
2023-01-31 13:26:59 +0300

    Create actions in the controller file.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
   def index
+    author = poster
+    @posts = Post.where(author)
   end
 
   def show
+    author = poster
+    post_id = params[ :id]
+    @post = Post.where(id: post_id, author:).order('id DESC').first
+    @comments = Comment.where(post_id: post_id)
+    @likes = Likes.where(post_id: post_id)
+  end
+
+  def poster
+    params[:user]
   end
 end
