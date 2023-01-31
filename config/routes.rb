693e508cb54464becdb7b2fca2416d5438ec7ccc
recillah khamala <recillahk@gmail.com>
2023-01-31 12:15:45 +0300

    Generate controller, view and route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :user only: [ :index, :show ]
   # Defines the root path route ("/")
   # root "articles#index"
 end
