5c6df4af67acf996e3d09ac6f1f0b79714471461
recillah khamala <recillahk@gmail.com>
2023-02-04 09:33:04 +0300

    Make styling changes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,html}'
+    './app/components/**/*.{erb,html}',
   ],
   theme: {
     extend: {
