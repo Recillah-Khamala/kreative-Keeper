e8160790fa20178a21a2bc724af9fe4ce7dd0888
recillah khamala <recillahk@gmail.com>
2023-02-10 14:19:37 +0300

    fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     users_url
   end
-  
+
   protected
- 
+
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :current_password)
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,7 +70,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Capybara::DSL
-  
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
