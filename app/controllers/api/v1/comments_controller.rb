d3cdb58a3894827205f9b8f0f430c6b556385f93
recillah khamala <recillahk@gmail.com>
2023-02-11 16:52:07 +0300

    fix linters errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,14 +1,13 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-   @user = User.find(params[:user_id])
-   @post = @user.posts.where(id: params[:post_id]).includes(:comments)[0]
-   
-   render json: @post.comments
+    @user = User.find(params[:user_id])
+    @post = @user.posts.where(id: params[:post_id]).includes(:comments)[0]
+
+    render json: @post.comments
   end
 
-  
-  def create 
-    @comment = Comment.new(comment_params.merge({author_id: current_user.id, post_id: params[:post_id]}))
+  def create
+    @comment = Comment.new(comment_params.merge({ author_id: current_user.id, post_id: params[:post_id] }))
 
     if @comment.save
       render json: @comment
@@ -17,7 +16,7 @@ class Api::V1::CommentsController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def comment_params
     params.require(:comment).permit(:text)
