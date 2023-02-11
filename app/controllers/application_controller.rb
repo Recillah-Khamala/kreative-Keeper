fe3405c1587e9c6c764e122d5eed9317ac869987
recillah khamala <recillahk@gmail.com>
2023-02-11 20:59:45 +0300

    Implement change requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :update_allowed_parameters, if: :devise_controller?
 
   # def after_sign_in_path_for(_resource)
   #   users_url
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,27 @@ Rails.application.routes.draw do
   
 
   # api related routes 
-  get 'api/v1/users/:id/posts', to: 'api/v1/posts#index'
-  get 'api/v1/users/:user_id/posts/:post_id/comments', to: 'api/v1/comments#index'
-  post 'api/v1/users/:user_id/posts/:post_id/comments', to: 'api/v1/comments#create'
-
+  # get 'api/v1/users/:id/posts', to: 'api/v1/posts#index'
+  # get 'api/v1/users/:user_id/posts/:post_id/comments', to: 'api/v1/comments#index'
+  # post 'api/v1/users/:user_id/posts/:post_id/comments', to: 'api/v1/comments#create'
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users do
+  #       resources :posts, only: [:index] do
+  #         resources :comments, only: [:index, :create]
+  #       end
+  #     end
+  #   end
+  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users do 
+        resources :posts 
+      end 
+      resources :posts do
+        resources :comments 
+        resources :likes 
+      end
+    end
+  end
 end
